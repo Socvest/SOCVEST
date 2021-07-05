c735adf5b57d243ca253f461679f0f599e38ccb2
Socvest <82335390+Socvest@users.noreply.github.com>
2021-07-05 02:20:28 +0100

    Update SOCVEST_app.py

--- SOCVEST_app.py
+++ SOCVEST_app.py
@@ -575,7 +575,7 @@ elif choice_1 == "Data and Analysis":
 
             # define data to be used for this section
             # Get the select boxes that will be used for filtering the data. Load the filtered data and the pivoted datatable
-            data_col, Trans_data, data_type = Filter_COVID_Timeseries_Data(Data_filtering)
+            data_col, Trans_data, data_type = list(Filter_COVID_Timeseries_Data(Data_filtering))
             
             # create new labels for hide data
             data_mix_buttons = st.beta_columns([3,1,1])
