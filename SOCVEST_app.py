05a4c20362d6d6480310a057dcb0f78c833335c7
Socvest <82335390+Socvest@users.noreply.github.com>
2021-07-11 16:29:36 +0100

    Update SOCVEST_app.py

--- SOCVEST_app.py
+++ SOCVEST_app.py
@@ -17,7 +17,10 @@ import urllib
 # for min_max scaling
 from sklearn.preprocessing import MinMaxScaler
 from streamlit.report_thread import get_report_ctx
-st.set_page_config(page_title='SOCVEST') #,layout="wide")
+from PIL import Image
+
+st.set_page_config(page_title='SOCVEST', page_icon =Image.open("img/page icon.png"))
+#st.set_page_config(page_title='SOCVEST') #,layout="wide")
 import datetime
 import calendar
 
